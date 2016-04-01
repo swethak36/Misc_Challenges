e62c9fea02c18819dd39e2277143251ad45e0786
swethak36 <swethak36@gmail.com>
2016-03-31 22:45:26 -0700

    Ramanujan Number

---
+++ Ramanujan.java
@@ -0,0 +1,39 @@
+public class Ramanujan {
+
+    public static void main(String[] args) { 
+
+        // read in one command-line argument
+        int N = Integer.MAX_VALUE;
+
+        // for each a, b, c, d, check whether a^3 + b^3 = c^3 + d^3
+        for (int a = 1; a <= N; a++) {
+            int a3 = a*a*a;
+            if (a3 > N) break;
+
+            // start at a to avoid print out duplicate
+            for (int b = a; b <= N; b++) {
+                int b3 = b*b*b;
+                if (a3 + b3 > N) break;
+
+                // start at a + 1 to avoid printing out duplicates
+                for (int c = a + 1; c <= N; c++) {
+                    int c3 = c*c*c;
+                    if (c3 > a3 + b3) break;
+
+                    // start at c to avoid printing out duplicates
+                    for (int d = c; d <= N; d++) {
+                        int d3 = d*d*d;
+                        if (c3 + d3 > a3 + b3) break;
+
+                        if (c3 + d3 == a3 + b3) {
+                            System.out.print((a3+b3) + " = ");
+                            System.out.print(a + "^3 + " + b + "^3 = "); 
+                            System.out.print(c + "^3 + " + d + "^3"); 
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
